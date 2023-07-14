d7f3c54da3c55d18a4affee8877dc086eb24b333
Константин Cоловьев <K1kollov@gmail.com>
2023-07-14 16:32:21 +0300

    s

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import List from "./components/list/list";
 import GamePage from "./pages/game/game";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<List />}></Route>
           <Route path="/game/:id" element={<GamePage />}></Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
